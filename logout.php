930a943985a2ad3e55ce0476a2bb77f04bda4303
pham minh <190164on@yse-c.net>
2020-10-16 09:10:48 +0900

    lol
    Signed-off-by: pham minh <190164on@yse-c.net>

--- logout.php
+++ logout.php
@@ -3,9 +3,9 @@
 【機能】
 	セッション情報を削除しログイン画面に遷移する。
 */
-session_start();
-unset($_SESSION['user']);
-header("Location: http://localhost/yse2019_00/login.php");
+//session_start();
+//unset($_SESSION['user']);
+//header("Location: http://localhost/yse2019_00/login.php");
 //①セッションを開始する。
 
 //②セッションを削除する。
